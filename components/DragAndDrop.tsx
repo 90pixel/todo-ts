a08ef3e72d25fa2ea18ab94f4cd9bca7219dc03d
Burak <burak@90pixel.com>
2022-03-17 16:23:37 +0300

    Add components folder.

---
+++ components/DragAndDrop.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+interface DragAndDropProps {
+  name?: string;
+}
+
+const DragAndDrop: FC<DragAndDropProps> = ({ name }) => {
+  return <div>X</div>;
+};
+
+export default DragAndDrop;
