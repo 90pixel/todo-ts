b78dd54d902eeecc8ee8628419c93bfe9d894aeb
eakyel <e.akyel@outlook.com>
2022-05-11 16:34:30 +0300

    Add components to index file

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,8 @@
-export { default as DndComponent } from './DndComponent';
+export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
-export { default as Modal } from './Modal';
 export { default as Collapse } from './Collapse';
+export { default as DndComponent } from './DndComponent';
+export { default as Modal } from './Modal';
 export { default as TodoItem } from './TodoItem';
+export { Text, Title } from './Typography';
+export { default as Textarea } from './Textarea';
