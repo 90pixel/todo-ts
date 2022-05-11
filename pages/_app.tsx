1163b6807783241392598325a0c992623dec71ff
eakyel <e.akyel@outlook.com>
2022-05-11 16:34:46 +0300

    Create Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app';
+import Layout from 'layout';
 import { globalStyles } from 'styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
