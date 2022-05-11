a1d274170be57d3e59d53cdca619b39470d54490
eakyel <e.akyel@outlook.com>
2022-05-11 16:35:01 +0300

    Create Dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,7 @@
-import Textarea from 'components/Textarea';
 import type { NextPage } from 'next';
-import { styled } from 'stitches.config';
-import { DndComponent } from 'components';
-import { Text, Title } from 'components/Typography';
+import { Dashboard } from 'modules';
 
 const Home: NextPage = () => {
-  const items = [
-    { id: '1', item: 'Denenme6' },
-    { id: '2', item: 'Denenme5' },
-    { id: '3', item: 'Denenme4' },
-    { id: '4', item: 'Denenme3' },
-    { id: '5', item: 'Denenme2' },
-    { id: '6', item: 'Denenme1' },
-  ];
-
-  return (
-    <div>
-      <Title>90Pixel ToDo</Title>
-      <Text>typography</Text>
-      <CustomButton>butt on</CustomButton>
-      <DndComponent
-        data={items}
-        onDragEnd={(e) => console.log(e)}
-        customDragIcon={<strong>Drag me!</strong>}
-      />
-    </div>
-  );
+  return <Dashboard />;
 };
-
 export default Home;
-
-const CustomButton = styled('button', {
-  background: '$gray800',
-  radius: 4,
-});
